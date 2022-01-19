2d1791f1dd108006182f2a611f38e4dbeeaf0e3e
Abdullahuysal <71669865+Abdullahuysal@users.noreply.github.com>
2022-01-19 17:22:26 +0300

    First homework

---
+++ Odevler/Odev1/Odev1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Odev1
+{
+    class Program
+    {
+        static List<string> model = new List<string> { "mercedes", "tesla", "bmw", "bentley", "audi", "bugatti", "porsche", "toyota" };
+        static bool Find(string marka)
+        {
+            bool deger = model.Contains(marka);
+            if (deger)
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Aramak istediğiniz modeli giriniz");
+            string marka = Console.ReadLine();
+            bool bulundumu = Find(marka);
+
+            if (bulundumu)
+            {
+                int index = model.IndexOf(marka) + 1;
+
+                Console.WriteLine("listede " + index + ". Sırada bulunmaktadır.");
+                Console.WriteLine("Tüm markalar:");
+
+                foreach (var item in model)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            string tarih = DateTime.Now.ToString();
+            string[] splittarih = tarih.Split(':');
+            string[] ay = splittarih[0].Split('/');
+            Console.WriteLine("bugün ayın " + ay[1] + "'u");
+
+        }
+    }
+}
