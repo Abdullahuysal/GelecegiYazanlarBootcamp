d2ba3acb42620a73e09e0daa9bb00f2ff27026eb
Abdullahuysal <71669865+Abdullahuysal@users.noreply.github.com>
2022-04-04 14:51:42 +0300

    Dapper HomeWork

---
+++ DapperHomeWork/DapperHomeWork/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using DapperHomeWork.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperHomeWork.Repositories
+{
+    public interface IProductRepository
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        string Add(Product product);
+        string Delete(int productId);
+    }
+}
---
+++ DapperHomeWork/DapperHomeWork/Repositories/ProductRepository.cs
@@ -0,0 +1,68 @@
+﻿using Dapper;
+using DapperHomeWork.Model;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperHomeWork.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        string sqlcon = "Data Source=DESKTOP-2DE1HM4\\SQLEXPRESS;Database=DapperIntro;Integrated Security=True";
+        public string Add(Product product)
+        {
+            using (IDbConnection db = new SqlConnection(sqlcon))
+            {
+                var rowsaffected=db.Execute(@"INSERT INTO [dbo].[Products]
+                        ([ProductName]
+                        ,[ProductQuantity]
+                        ,[ProductPrice])
+                        VALUES
+                        (@ProductName,
+                         @ProductQuantity,
+                         @ProductPrice)",
+                         new
+                         {
+                             product.ProductName,
+                             product.ProductQuantity,
+                             product.ProductPrice
+                         });
+                return Messages.AddSuccesfull;
+            }
+          
+                
+        }
+
+        public string Delete(int productId)
+        {
+            using (IDbConnection db = new SqlConnection(sqlcon))
+            {
+                var sqlstate = "Delete Products Where ProductId=@ProductId";
+                db.Execute(sqlstate, new { ProductId = productId });
+            }
+            return Messages.DeleteSuccessfull;
+        }
+
+        public List<Product> GetAll()
+        {
+            using (IDbConnection db = new SqlConnection(sqlcon))
+            {
+                
+                return db.Query<Product>("Select * FROM Products").ToList();
+            }
+            
+        }
+
+        public Product GetById(int ProductId)
+        {
+            using (IDbConnection db = new SqlConnection(sqlcon))
+            {
+                return db.Query<Product>("Select * From Products Where ProductId=@ProductId",new {ProductId=ProductId}).SingleOrDefault();
+            }
+        }
+    }
+}
